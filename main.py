ab3f54aa6d626b063f99ad556f06bd020b6a2e22
HiddEnigma <chaoticassembly@hotmail.com>
2022-07-21 17:39:17 -0300

    (Update) - Add functionality. Uses Google's gTTS to transform PDF into text. Warning: Seems unreliable.

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from gtts import gTTS
+from PyPDF2 import PdfReader
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pdfplumber as pdfplumber
+import os
 
+Tk().withdraw()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+file_location = askopenfilename()
 
+file_name = os.path.basename(file_location)
+name = os.path.splitext(file_name)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+try:
+    pdf_file = open(file_location, "rb")
+except:
+    print("That didn't work.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+reader = PdfReader(pdf_file)
+all_text = ""
+
+for page in reader.pages:
+    all_text += page.extract_text()
+
+tts = gTTS(text=all_text, lang="en")
+tts.save(f"{name[0]}.mp3")
